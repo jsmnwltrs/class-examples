b94e68d8e3cd11d5d074332c01119546acdb1cee
Jasmine Walters <jsmnwltrs@gmail.com>
2019-03-02 13:50:32 -0600

    added some stuff for candy

---
+++ ClassesExamples/Candy.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassesExamples
+{
+    class Candy
+    {
+        //Properties
+        public string Flavor { get; }
+        public int Rating { get; private set; }
+        //you can set an initial value to a property. its called property initialization.
+        //public int Rating { get; private set; } = 1
+        public string Name { get; }
+        public CandyType Type { get; }
+        public string Review { get; private set; }
+
+        //Fields
+        const int MinRatingValue = 1; //Constant fields are named with Capitals.
+        const int MaxRatingValue = 10;
+
+        //Constructor
+        public Candy(string flavor, string name, CandyType type)
+        {
+            Flavor = flavor;
+            Name = name;
+            Type = type;
+        }
+
+        //Methods (function inside a class)
+        public void SetRating(int userRating) //void is a return type that means this method won't return anything. Method signature: accessiblity, return type, name, parameter list
+        {
+            if (userRating >= MinRatingValue && userRating <= MaxRatingValue)
+            {
+                Rating = userRating;
+            }
+            else
+            {
+                throw new Exception($"You are dumb. Ratings are {MinRatingValue}-{MaxRatingValue} moron.");
+            }
+        }
+
+        public void SetRating(int userRating, string review)
+        {
+            SetRating(userRating);
+            Review = review;
+        }
+
+        public override string ToString()
+        {
+            return $"This is {Type} {Name}, with a(n) {Flavor} flavor, rated {Rating} with a review of {Review}";
+        }
+
+        public 
+
+    }
+    enum CandyType //This is basically an options class.
+    {
+        Chocolate = 1, //You can set a number if you don't want Chocolate to be defaulted. In this case if 0 is returned then a type hasn't been given.
+        HardCandy,
+        Caramel,
+        Sour,
+        Stretchy,
+        Nuts
+    }
+}
--- ClassesExamples/Program.cs
+++ ClassesExamples/Program.cs
@@ -4,9 +4,28 @@ namespace ClassesExamples
 {
     class Program
     {
-        static void Main(string[] args)
+        //Public can be accessed from anywhere, Private can't be accessed from anywhere. Kinda like global and local variables.
+
+        //string _myData = "this is just for me to use"; //This is a field and it is kinda like a private state
+        //public int Number { get; set; } //This is a property and it is kinda like a public state
+
+        //public void DoTheThing()
+        //{
+        //Number = 1; //set Number
+        //var x = Number; //get Number
+        //}
+
+
+        static void Main(string[] args) //The static method is also private
         {
-            Console.WriteLine("Hello World!");
+            var taffy = new Candy("Orange", "Orange Saltwater Taffy", CandyType.Stretchy);
+
+            taffy.SetRating(10);
+            taffy.SetRating(5, "kinda liked it");
+
+            Console.WriteLine(taffy);
+ 
         }
+        
     }
 }
